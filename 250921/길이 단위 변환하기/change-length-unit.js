19ea6fd8dc64860a4326916fa4986f330edda792
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-21 18:13:48 +0900

    Title: 길이 단위 변환하기, Time: 59ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250921/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,5 @@
+let nine_to_two_ft = 9.2 * 30.48;
+let one_to_three_mile = 1.3 * 160934;
+
+console.log("9.2ft = " + nine_to_two_ft.toFixed(1)+"cm");
+console.log("1.3mi = " + one_to_three_mile.toFixed(1)+"cm");
