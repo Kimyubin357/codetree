35dc67d8d582027afaea358b301f87753a16bfde
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-21 18:09:08 +0900

    Title: 달에서 무게 구하기, Time: 57ms, Memory: 10MB, Status: Passed - Codetree

--- 250921/달에서 무게 구하기/weight-on-the-moon.js
+++ 250921/달에서 무게 구하기/weight-on-the-moon.js
@@ -1,4 +1,4 @@
 let chu = 13;
 let moon_rate = 0.165;
 let result = chu * moon_rate;
-console.log(`${chu} * ${moon_rate.toFixed(6)} = ${result.toFixed(6)});
+console.log(`${chu} * ${moon_rate.toFixed(6)} = ${result.toFixed(6)}`);
