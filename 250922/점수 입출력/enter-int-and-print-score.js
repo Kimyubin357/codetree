4a881beee266b60da3da36f3415729046d290294
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-22 13:26:09 +0900

    Title: 점수 입출력, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 250922/점수 입출력/enter-int-and-print-score.js
+++ 250922/점수 입출력/enter-int-and-print-score.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let input = fs.readFileSync(0).toString();
-input = Number(input);
+let input = fs.readFileSync(0).toString().trim();
 
-console.log(`Your socre is ${input} point.`);
+
+console.log(`Your score is ${input} point.`);
