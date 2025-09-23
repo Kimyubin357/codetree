a34ceea3912313957f2b563c7c31984d64704169
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-23 13:45:36 +0900

    Title: 음수 구별하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250923/음수 구별하기/separate-negative-number.js
+++ 250923/음수 구별하기/separate-negative-number.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-let input = fs.readFileSync(0).toString().trim().map(Number);
-
-console.log(input);
-if(input < 0)
+let input = fs.readFileSync(0).toString().trim();
+let n = Number(input);
+console.log(n);
+if(n < 0)
     console.log('minus');
