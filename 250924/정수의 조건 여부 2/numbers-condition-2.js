e60f19d3d386af8ee609d6c68151a5b8d7b6c9fe
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-24 21:55:32 +0900

    Title: 정수의 조건 여부 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250924/정수의 조건 여부 2/numbers-condition-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+let a = Number(fs.readFileSync(0).toString());
+
+if(a == 5){
+    console.log('A');
+}else if(a % 2 == 0){
+    console.log('B');
+}else{
+    console.log('')
+}
