caa74eb096c96d0f2c5fd32d6fbbe2d9a0d52cde
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-24 10:03:49 +0900

    Title: 정사각형의 넓이, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250924/정사각형의 넓이/area-of-a-rectangle.js
+++ 250924/정사각형의 넓이/area-of-a-rectangle.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let n = fs.readFileSync(0).toString().trim().map(Number);
-
+let n = fs.readFileSync(0).toString().trim();
+n = Number(n);
 console.log(n * n);
 
 if(n < 4){
