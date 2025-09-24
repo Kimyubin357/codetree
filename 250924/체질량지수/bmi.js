7c2808c7a94e06cf68844d5aa0df963289b5f77f
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-24 10:01:17 +0900

    Title: 체질량지수, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 250924/체질량지수/bmi.js
+++ 250924/체질량지수/bmi.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 let [h, w] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let result = 10000 * w / h * h;
+let result = (10000 * w )/ (h * h);
 
-console.log(result)
+console.log(parseInt(result))
+if(result >= 25) console.log('Obesity');
