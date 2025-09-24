f0efbb7a12a55bcd0ead3e2c5f6c6b1fed99f30b
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-24 16:28:15 +0900

    Title: 등급 매기기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 250924/등급 매기기/ranking.js
+++ 250924/등급 매기기/ranking.js
@@ -3,14 +3,19 @@ const fs = require('fs');
 let n = fs.readFileSync(0).toString().trim();
 
 n = Number(n);
-let result ='';
+
+let result;
+
 if(n >= 90){
-    result += 'A';
-} 
-else if(n >= 80) {result += 'B';}
-else if(n >= 70) {result += 'C';}
-else if(n >= 60) {result += 'D';}
-else {
-    result += 'D';
+    result = 'A';
+}else if(n >= 80){
+    result = 'B';
+}else if(n >= 70){
+    result = 'C';
+}else if(n >= 60){
+    result = 'D';
+}else{
+    result = 'F';
 }
+
 console.log(result);
