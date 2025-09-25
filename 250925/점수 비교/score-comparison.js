2ee2c816c1d4681ba123a1da1875ccb1f8119f97
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 12:56:23 +0900

    Title: 점수 비교, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/점수 비교/score-comparison.js
+++ 250925/점수 비교/score-comparison.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 
 let [A, B] = fs.readFileSync(0).toString().trim().split('\n');
 
-let [math_A, eng_A] = A.split(' ');
-let [math_B, eng_B] = B.split(' ');
+let [math_A, eng_A] = A.split(' ').map(Number);
+let [math_B, eng_B] = B.split(' ').map(Number);
+
 
 if(math_A > math_B && eng_A > eng_B){
     console.log(1);
 }else{
     console.log(0);
 }
-
 
