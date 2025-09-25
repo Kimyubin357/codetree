b44cd9f83d3382e30e7fd62e831f273a55ac4b11
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 13:13:21 +0900

    Title: 세 정수의 최솟값, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 250925/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -4,4 +4,8 @@ let [a, b, c]= fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 if(a <= b && a <= c){
     console.log(a);
+}else if(b <= a && b <= c){
+    console.log(b);
+}else if(c <= b && c <= a){
+    console.log(c);
 }
