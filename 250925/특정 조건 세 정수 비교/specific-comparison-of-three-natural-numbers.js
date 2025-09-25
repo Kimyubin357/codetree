71e02f50198f4b059a04649b3cc3122f497d0e62
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 13:09:33 +0900

    Title: 특정 조건 세 정수 비교, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250925/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 250925/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
 let [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
-
+let result;
 if(a < b && a < c){
-    console.log(1);
+    result = 1 + ' ';
 }else{
-    console.log(0);
+    result = 0 + ' ';
 }
 
 if(a == b && b == c && c == a){
-    console.log(1);
+    result += 1;
 }else{
-    console.log(0);
+    result += 0;
 }
