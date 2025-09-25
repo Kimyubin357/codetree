31396b1957cfe05a6ff06d5135d03feda146da14
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 15:08:11 +0900

    Title: 좀 더 어려운 수학 점수, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250925/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+let [A, B] = fs.readFileSync(0).toString().trim().split('\n');
+
+let [math_A, eng_A] = A.split(' ').map(Number);
+let [math_B, eng_B] = B.split(' ').map(Number);
+
+
+if(math_A > math_B){
+    console.log('A');
+}else if(math_B > math_A){
+    console.log('B');
+}else{
+    if(eng_A > eng_B){
+        console.log('A');
+    }else {
+        console.log('B');
+    }
+}
+
+
+
