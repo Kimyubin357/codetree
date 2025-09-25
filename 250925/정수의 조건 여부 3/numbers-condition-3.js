d098ce0b43dec63334f7c2bfb01e97a43fbce2e6
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 13:21:23 +0900

    Title: 정수의 조건 여부 3, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/정수의 조건 여부 3/numbers-condition-3.js
+++ 250925/정수의 조건 여부 3/numbers-condition-3.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let a = Number(fs.readFileSync(0).toString().trim());
 
 if(a % 13 == 0 || a % 19 == 0){
-    console.log(True);
+    console.log('True');
 }else{
-    console.log(False);
+    console.log('False');
 }
