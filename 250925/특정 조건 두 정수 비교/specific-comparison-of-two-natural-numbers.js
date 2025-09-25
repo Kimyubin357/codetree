293b4b9117b0846b00ba67fd227c1a7016a7c4b2
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 11:15:36 +0900

    Title: 특정 조건 두 정수 비교, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250925/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
+++ 250925/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -2,14 +2,17 @@ const fs = require('fs');
 
 let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
+let result;
 if(a < b){
-    console.log(1);
+    result += '1' + ' ';
 }else{
-    console.log(0);
+    result += '0' + ' ';
 }
 
 if(a == b){
-    console.log(1);
+    result += '1';
 }else{
-    console.log(0);
+    result += '0';
 }
+
+console.log(result);
