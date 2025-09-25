191025cacd0d48f5d69db93b20dd2306549ae89d
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-25 15:30:03 +0900

    Title: 두 사람, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/두 사람/two-person.js
+++ 250925/두 사람/two-person.js
@@ -8,9 +8,14 @@ let [B_age, B_gen] = B.trim().split(' ');
 A_age = Number(A_age);
 B_age = Number(B_age);
 
+
 if(A_age >= 19 || B_age >= 19){
-    if(A_gen === 'M' || B_gen === 'M'){
+    if(A_age >= 19 && A_gen == 'M'){
         console.log(1);
+    }else if(B_age >= 19 && B_gen == 'M'){
+        console.log(1);
+    }else {
+        console.log(0);
     }
 }else{
     console.log(0);
@@ -18,4 +23,4 @@ if(A_age >= 19 || B_age >= 19){
 
 //두사람 중 한 사람이라도 19세 이상이면서
 
-//한 사람이라도 남자라면?
+//그 19세 이상인 한 사람이 남자라면?
