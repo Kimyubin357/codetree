4174264be3a0bba4fc711d700d715e9349ee1e59
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-26 18:22:42 +0900

    Title: 남녀노소 구분짓기, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250926/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let [gen, age] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+if(gen == 0){
+    if(age >= 19){
+        console.log('MAN');
+    }else{
+        console.log('BOY');
+    }
+}else{
+    if(age >= 19){
+        console.log('WOMAN');
+    }else{
+        console.log('GIRL');
+    }
+}
