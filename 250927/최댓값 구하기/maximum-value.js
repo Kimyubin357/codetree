6ba0112acc48cc6f2084df978a026d291879e168
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-27 22:38:51 +0900

    Title: 최댓값 구하기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250927/최댓값 구하기/maximum-value.js
+++ 250927/최댓값 구하기/maximum-value.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 let [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-if(a > b && a > c){
+if(a >= b && a >= c){
     console.log(a)
-}else if(b > a && b >> c){
+}else if(b >= a && b >= c){
     console.log(b)
-}else if(c > a && c >> b){
+}else if(c >= a && c >= b){
     console.log(c)
 }
