565b57222b6611efa4b3e79b94572d53e56b5642
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-28 23:03:57 +0900

    Title: 1부터 n까지 출력, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 250928/1부터 n까지 출력/print-from-1-to-n.js
+++ 250928/1부터 n까지 출력/print-from-1-to-n.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let n = Number(fs.readFileSync(0).toString().trim);
+let n = Number(fs.readFileSync(0).toString().trim());
 
 let result = '';
 for(let i = 1 ; i <= n; i++){
