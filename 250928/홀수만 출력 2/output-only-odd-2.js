624901cc33396e760fcbfdf3aba0e72cc039affd
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-28 23:16:25 +0900

    Title: 홀수만 출력 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250928/홀수만 출력 2/output-only-odd-2.js
+++ 250928/홀수만 출력 2/output-only-odd-2.js
@@ -4,7 +4,7 @@ let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 let result ='';
 
-for(let i = a; i >= b; i--){
+for(let i = a; i >= b; i-=2){
     result += i + ' ';
 }
 
