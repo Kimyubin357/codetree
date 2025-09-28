1ea76a83d464f82eb2957d623cae9992c70c3834
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-28 23:07:48 +0900

    Title: 정수 입력받아 배수 출력, Time: 61ms, Memory: 10MB, Status: Passed - Codetree

--- 250928/정수 입력받아 배수 출력/print-multiple-of-input.js
+++ 250928/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 let n = Number(fs.readFileSync(0).toString().trim());
 let result = '';
-for(let i = 0; i < 5; i++){
+for(let i = 1; i <= 5; i++){
     result += (i * n) + ' ';
 }
 console.log(result);
