10e293f52d97872e221f191a6d3d4a4471c88b65
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-30 00:56:19 +0900

    Title: 높은 수에서 낮은 수까지, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 250930/높은 수에서 낮은 수까지/from-high-to-low.js
+++ 250930/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 let result = '';
-for(let i = b; i >= a; i--){
-    result += i + ' ' ;
+if(b > a){
+    for(let i = b; i >= a; i--){
+        result += i + ' ' ;
+    }
+}else{
+    for(let i = a; i >= b; i--){
+        result += i + ' ' ;
+    }
 }
+
 
 console.log(result);
