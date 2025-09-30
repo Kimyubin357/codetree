904f8f5963e9c5fc51df1d01e2069929118e31b2
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-09-30 22:48:07 +0900

    Title: 369 게임, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250930/369 게임/369-game.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let n = Number(fs.readFileSync(0).toString());
+let i = 1;
+let result = '';
+while(i <= n){
+    if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9){
+        result += 0 + ' ';
+    }else{
+        result += i + ' ';
+    }
+    i++;
+}
+console.log(result);
