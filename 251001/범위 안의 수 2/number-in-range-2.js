db106d644e1de3769e9c8e821e05920b3e94576b
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 23:07:02 +0900

    Title: 범위 안의 수 2, Time: 60ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251001/범위 안의 수 2/number-in-range-2.js
+++ 251001/범위 안의 수 2/number-in-range-2.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 
 let n = readFileSync(0).toString().trim().split('\n').map(Number);
 let sum = 0, cnt = 0;
+let avg;
 for(let i = 0; i < 10; i++){
     if(n[i] >= 0 && n[i] <= 200){
         sum += i;
         cnt++;
     }
 }
-console.log(sum, (sum / cnt).toFixed(1))
+avg = sum / cnt;
+console.log(sum, avg.toFixed(1))
