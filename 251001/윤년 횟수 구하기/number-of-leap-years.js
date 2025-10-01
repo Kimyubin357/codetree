4a7751ccd1a9dd2b9eb75f8f7becee05bc0b551f
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 22:14:45 +0900

    Title: 윤년 횟수 구하기, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251001/윤년 횟수 구하기/number-of-leap-years.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let n = Number(fs.readFileSync(0).toString());
+let cnt = 0;
+while(n > 0){
+    if(n % 4 === 0){
+        if(n % 100 !== 0 && n % 400 === 0){
+            cnt++;
+        }
+    }
+    n--;
+}
+console.log(cnt);
