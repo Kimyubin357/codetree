8e95cd6434acc25c649e6b568f4902322bad57fb
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 23:10:19 +0900

    Title: 범위 안의 수 2, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 251001/범위 안의 수 2/number-in-range-2.js
+++ 251001/범위 안의 수 2/number-in-range-2.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-let n = readFileSync(0).toString().trim().split('\n').map(Number);
+let n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 let sum = 0, cnt = 0;
 let avg;
 for(let i = 0; i < 10; i++){
     if(n[i] >= 0 && n[i] <= 200){
-        sum += i;
+        sum += n[i];
         cnt++;
     }
 }
