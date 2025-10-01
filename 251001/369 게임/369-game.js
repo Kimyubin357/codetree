8dc1b1b53ccad7430eab74b2554b01cde8ace379
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 20:57:36 +0900

    Title: 369 게임, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251001/369 게임/369-game.js
+++ 251001/369 게임/369-game.js
@@ -4,7 +4,7 @@ let n = Number(fs.readFileSync(0).toString());
 let i = 1;
 let result = '';
 while(i <= n){
-    if(i % 3 === 0 || ( i % 10 === 3 || i % 10 === 6 || i % 10 === 9)){
+    if(i % 3 === 0 || ( i % 10 === 3 || i % 10 === 6 || i % 10 === 9) || (parseInt(i / 10) === 3 || parseInt(i / 10) === 6 || parseInt(i/10) === 9)){
         result += 0 + ' ';
     }else{
         result += i + ' ';
