f5163cd5b45ed517bb2fc945d728aedac3329046
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 22:41:39 +0900

    Title: 홀수만 더하기, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251001/홀수만 더하기/add-only-odd-numbers.js
+++ 251001/홀수만 더하기/add-only-odd-numbers.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
 let n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+console.log(n);
 let sum = 0;
-for(let i = 1; i <= n; i++){
+for(let i = 1; i <= n[0]; i++){
     if(n[i] % 2 === 1 && n[i] % 3 === 0){
         sum += n[i];
     }
 }
 
-console.log(sum);
+// console.log(sum);
