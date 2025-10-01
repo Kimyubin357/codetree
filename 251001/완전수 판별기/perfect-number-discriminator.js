7f87aa95616f7c132fd85917f3ba17bcce22825c
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-01 23:19:08 +0900

    Title: 완전수 판별기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 251001/완전수 판별기/perfect-number-discriminator.js
+++ 251001/완전수 판별기/perfect-number-discriminator.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
 let n = Number(fs.readFileSync(0).toString());
-let i = 1;
+let i = 0;
 let sum = 0;
-while(i <= n){
+for(let i = 1 ; i < n; i++){
     if(n % i === 0){
         sum += i;
     }
-    i++;
 }
-console.log(sum)
+
 if(n == sum){
     console.log('P');
 }else{
