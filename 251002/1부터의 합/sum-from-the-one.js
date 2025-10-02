e606f16f646e23af283923257055a350feeb602c
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-02 15:22:22 +0900

    Title: 1부터의 합, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251002/1부터의 합/sum-from-the-one.js
+++ 251002/1부터의 합/sum-from-the-one.js
@@ -4,10 +4,10 @@ let n = Number(fs.readFileSync(0).toString());
 
 let sum = 0;
 for(let i = 1; i <= 100; i++){
-    sum += i;
-    if(sum >= n){
+    if(sum + i >= n){
         break;
     }
+    sum += i;
 }
 
 console.log(sum);
