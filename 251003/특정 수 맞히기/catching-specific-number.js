402e3ec077faed76a8a028cc46bebf96b760ece1
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-03 21:24:59 +0900

    Title: 특정 수 맞히기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/특정 수 맞히기/catching-specific-number.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+let i = 0;
+
+while(true){
+    if(n[i]===25){
+        console.log("Good");
+        break;
+    }else if(n[i]>25){
+        console.log("Lower");
+    }else{
+        console.log("Higher");
+    }
+    i++;
+}
