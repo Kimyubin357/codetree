0128e7b0ab1d6f6a839ee669f31e821a18b5c7db
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-03 21:35:45 +0900

    Title: 출석 부르기 2, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251003/출석 부르기 2/calling-attendance-2.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+let n = fs.readFileSync(0).toString().trim().split('\n');
+let i = 0;
+while(true){
+    if(n[i]==1){
+        console.log('John');
+    }else if(n[i]==2){
+        console.log('Tom');
+    }else if(n[i]==3){
+        console.log('Paul');
+    }else if(n[i]==4){
+        console.log('Sam');
+    }else{
+        console.log('Vacancy');
+        break;
+    }
+    i++;
+}
