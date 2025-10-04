7f8f5188fccb8011c53b7a93d3e37536077e09c3
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-04 22:24:57 +0900

    Title: 몫 출력하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251004/몫 출력하기/print-share.js
+++ 251004/몫 출력하기/print-share.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 
 let i = 0;
-while(i <= 3){
+while(n[i] != undefined){
     if(n[i] % 2 == 0){
         n[i] = parseInt(n[i] / 2);
         console.log(n[i]);
