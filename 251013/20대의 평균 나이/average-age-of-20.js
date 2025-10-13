d72ad002098b9437794f02b64217a786f793db21
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-13 14:21:00 +0900

    Title: 20대의 평균 나이, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251013/20대의 평균 나이/average-age-of-20.js
+++ 251013/20대의 평균 나이/average-age-of-20.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 
 let n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 let i = 0;
-let avg = 0;
+let sum = 0;
 let cnt = 0;
 while(n[i] < 30){
-    avg += n[i];
+    sum += n[i];
     cnt++;
     i++;
 }
-console.log((avg / cnt).toFixed(2));
+let avg = sum / cnt ;
+console.log(avg.toFixed(2));
