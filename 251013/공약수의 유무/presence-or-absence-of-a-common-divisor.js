c22f83aa611776c39ee94b3d93cc682ec31396d9
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-13 14:53:36 +0900

    Title: 공약수의 유무, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251013/공약수의 유무/presence-or-absence-of-a-common-divisor.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+let cnt = 0;
+for(let i = a; i <= b; i++){
+    if(1920 % i === 0 && 2880 % i === 0) cnt++;
+}
+console.log(cnt);
