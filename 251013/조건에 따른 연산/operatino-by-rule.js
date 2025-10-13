ce0926c66ee7c70119a510553968140975c03ae4
Yubin Kim <164238455+Kimyubin357@users.noreply.github.com>
2025-10-13 14:43:37 +0900

    Title: 조건에 따른 연산, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251013/조건에 따른 연산/operatino-by-rule.js
+++ 251013/조건에 따른 연산/operatino-by-rule.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 let n = Number(fs.readFileSync(0).toString().trim());
 let cnt = 0;
-while(n > 1000){
+while(n < 1000){
     if(n % 2 === 0){
         n = 3 * n + 1;
     }else{
